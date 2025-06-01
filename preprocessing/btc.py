22038f7bc5147725fff9d67af2f702776fc7091d
mockingjae <macqueen01@berkeley.edu>
2025-06-01 15:21:14 +0900

    Fix: small bug fixed

--- preprocessing/btc.py
+++ preprocessing/btc.py
@@ -1,24 +1,25 @@
-from genericpath import isfile
 import os
 import numpy as np
 import pandas as pd
 import kagglehub
+from tqdm import tqdm
 
 import constants as cst
-
 class BTCDataBuilder:
     def __init__(
         self, 
         data_dir: str,
         sampling_time: cst.SamplingTime,
         dates: list[str],
-        split_rates: list[float]
+        split_rates: list[float],
+        split_days: list[int]
     ): 
         self.n_lob_levels = cst.N_LOB_LEVELS
         self.data_dir = data_dir
         self.sampling_time = sampling_time
         self.dates = dates
         self.split_rates = split_rates
+        self.split_days = split_days
         self.train_set = None
         self.val_set = None
         self.test_set = None
@@ -35,12 +36,15 @@ class BTCDataBuilder:
         if len(os.listdir(save_dir)) == 0:
             print("Downloading BTC - USDT SPOT orderbook data from Kaggle ...")
             path = kagglehub.dataset_download(
-                "macqueen01/btcusdt-orderbook", cst.RAW_DATA_DIR, force_download=True)
+                "macqueen01/btcusdt-orderbook", force_download=False)
             
             files = os.listdir(path)
-            self.num_trading_days = len(files)
             
-            for file_path in [os.path.join(path, file) for file in files]:
+            for file_path, file_name in tqdm(
+                [(os.path.join(path, file), file) for file in files], 
+                desc="Processing BTC orderbook files",
+                unit="files"
+            ):
                 dataframe = pd.read_csv(file_path)
                 # Gather timestamp, sell_price_#, sell_size_#, buy_price_#, buy_size_# columns
                 # Create sell-buy pairs for each level: sell_price_i, sell_size_i, buy_price_i, buy_size_i
@@ -51,14 +55,22 @@ class BTCDataBuilder:
                         f"buy_price_{i+1}", f"buy_size_{i+1}"
                     ]
                 dataframe = dataframe.loc[:, columns]
-                dataframe.to_csv(os.path.join(save_dir, file_path), index=False)
+                
+                save_path = os.path.join(save_dir, file_name)            
+                if os.path.exists(save_path): continue
+                
+                dataframe.to_csv(save_path, index=False)
             
+        self.num_trading_days = len(os.listdir(save_dir))
         self.dataframes = []
         self._prepare_dataframes(save_dir)
         
-        self.train_set = np.concatenate([self.dataframes[0].values, self.train_labels_horizons.values])
-        self.val_set = np.concatenate([self.dataframes[1].values, self.val_labels_horizons.values])
-        self.test_set = np.concatenate([self.dataframes[2].values, self.test_labels_horizons.values])
+        print(self.train_labels_horizons.values.shape)
+        print(self.dataframes[0].values.shape)
+        
+        self.train_set = np.concatenate([self.dataframes[0].values, self.train_labels_horizons.values], axis=-1)
+        self.val_set = np.concatenate([self.dataframes[1].values, self.val_labels_horizons.values], axis=-1)
+        self.test_set = np.concatenate([self.dataframes[2].values, self.test_labels_horizons.values], axis=-1)
         
         path_where_to_save = "{}/{}".format(
             self.data_dir,
@@ -72,14 +84,16 @@ class BTCDataBuilder:
         test_set = None
         [train_days, val_days, test_days] = self._split_days()
         
-        for i, file in enumerate(os.listdir(dir)):
-            if not os.path.isfile(file):
-                raise ValueError(f"File {file} is not a file")
+        for i, (file_path, file_name) in enumerate([(os.path.join(dir, file_name), file_name) for file_name in os.listdir(dir)]):
+            if not os.path.isfile(file_path):
+                raise ValueError(f"File {file_path} is not a file")
             
-            df_ob = pd.read_csv(file)
+            df_ob = pd.read_csv(file_path)
+            print(f"{file_name} has shape: ", df_ob.shape)
             df_ob = self._sample(df_ob)
             df_ob.drop(columns=['timestamp'])
             
+            # Issue: current data is not consecutive, so somehow we need to seperate...
             if i < train_days:
                 train_set = df_ob if train_set is None else pd.concat([train_set, df_ob])
             elif i < val_days:
@@ -101,21 +115,21 @@ class BTCDataBuilder:
                 self.dataframes[2], cst.LEN_SMOOTH, horizon)
             
             if i == 0:
-                self.train_labels_horizons = pd.DataFrame(train_labels, columns=[f"label_h{horizon}"])
-                self.val_labels_horizons = pd.DataFrame(val_labels, columns=[f"label_h{horizon}"])
-                self.test_labels_horizons = pd.DataFrame(test_labels, columns=[f"label_h{horizon}"])
+                self.train_labels_horizons = pd.DataFrame(train_labels, columns=[f"label_h{horizon}"], dtype=np.float64)
+                self.val_labels_horizons = pd.DataFrame(val_labels, columns=[f"label_h{horizon}"], dtype=np.float64)
+                self.test_labels_horizons = pd.DataFrame(test_labels, columns=[f"label_h{horizon}"], dtype=np.float64)
             else:
-                self.train_labels_horizons[f"label_h{horizon}"] = train_labels
-                self.val_labels_horizons[f"label_h{horizon}"] = val_labels
-                self.test_labels_horizons[f"label_h{horizon}"] = test_labels
+                self.train_labels_horizons[f"label_h{horizon}"] = train_labels.astype(np.float64)
+                self.val_labels_horizons[f"label_h{horizon}"] = val_labels.astype(np.float64)
+                self.test_labels_horizons[f"label_h{horizon}"] = test_labels.astype(np.float64)
         
         self._normalize_data()
         
     def _label_data(self, input: pd.DataFrame, window_size: int, horizon: int) -> np.ndarray:
         sell_price_over_window = np.lib.stride_tricks.sliding_window_view(
-            input[:,'sell_price_1'], window_shape=window_size)
+            input['sell_price_1'], window_shape=window_size)
         buy_price_over_window = np.lib.stride_tricks.sliding_window_view(
-            input[:,'buy_price_1'], window_shape=window_size)
+            input['buy_price_1'], window_shape=window_size)
         avg_price = (buy_price_over_window + sell_price_over_window) / 2
         smoothen_avg_price = avg_price.mean(axis=-1)
         
@@ -135,8 +149,7 @@ class BTCDataBuilder:
             np.where(percentage_change > alpha, 0, 1)
         )
         len_empty_timestamps = input.values.shape[0] - labels.shape[0]
-        # Pad the end with inf to indicate invalid/unavailable labels
-        return np.pad(labels, (0, len_empty_timestamps), mode='constant', constant_values=np.inf)
+        return np.concatenate([labels, np.full(shape=(len_empty_timestamps), fill_value=np.inf)])
 
     def _normalize_data(self):
         train_mean_price, train_std_price = self._get_stats_for_price(self.dataframes[0])
@@ -179,9 +192,9 @@ class BTCDataBuilder:
         return dataframe
         
     def _split_days(self) -> list[int]:
-        train = int(self.num_trading_days * self.split_rates[0])
-        val = int(self.num_trading_days * self.split_rates[1]) + train
-        test = int(self.num_trading_days * self.split_rates[2]) + val
+        train = self.split_days[0]
+        val = self.split_days[1] + train
+        test = self.split_days[2] + val
         print(f"There are {train} days for training, {val - train} days for validation and {test - val} days for testing")
         return [train, val, test]
     
@@ -189,18 +202,20 @@ class BTCDataBuilder:
         if (self.train_set is None or self.val_set is None or self.test_set is None):
             raise ValueError("Train, val or test set is None")
         
+        if not os.path.exists(path_to_save):
+            os.makedirs(path_to_save, exist_ok=True)
+            
         np.save(path_to_save + "/train.npy", self.train_set)
         np.save(path_to_save + "/val.npy", self.val_set)
         np.save(path_to_save + "/test.npy", self.test_set)
         
     def _sample(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'], errors='coerce')
-        # fill up missing snapshot with previous snapshot
-        dataframe = dataframe \
+        # Convert milliseconds since epoch to datetime
+        dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'], unit='ms', errors='coerce')
+        return dataframe \
             .set_index('timestamp') \
-            .resample(self.sampling_time.value) \
-            .first() \
-            .ffill().reset_index()
-        return dataframe
+            .resample(self.sampling_time.value).first() \
+            .ffill() \
+            .reset_index()
             
             
